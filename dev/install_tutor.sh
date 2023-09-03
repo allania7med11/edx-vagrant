2cf071a433af4a41bcff56b18b9050393815a0fb
allania7med11 <allania7med11@gmail.com>
2023-09-03 12:57:04 +0100

    update install tutor script

--- dev/install_tutor.sh
+++ dev/install_tutor.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 mkdir -p ~/openedx
 cd ~/openedx
-sudo apt install python3.8-venv
-python3.8 -m venv tutor-venv
+sudo apt update
+sudo apt install build-essential python3-dev -y
+sudo apt install python3.8-venv -y
+python3.8 -m venv tutor-venv 
 . tutor-venv/bin/activate
 git clone --branch=nightly https://github.com/overhangio/tutor.git
 pip install -e "./tutor[full]"
+pip install -r tutor/requirements/dev.txt
 tutor dev launch
