9dbf74173659b795f629799157a101415a73e570
allania7med11 <allania7med11@gmail.com>
2023-09-19 12:23:48 +0100

    Update install_tutor.sh

--- dev/install_tutor.sh
+++ dev/install_tutor.sh
@@ -2,7 +2,7 @@
 mkdir -p ~/openedx
 cd ~/openedx
 sudo apt update
-sudo apt install build-essential python3-dev -y
+sudo apt install build-essential python3.8-dev -y
 sudo apt install python3.8-venv -y
 python3.8 -m venv tutor-venv 
 . tutor-venv/bin/activate
